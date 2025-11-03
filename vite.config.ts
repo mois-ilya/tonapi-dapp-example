89dcbe7cf3ec9bd49815e9693e48c7e84eaf2f3d
Moiseev Ilya <ilya@mois.pro>
2025-11-03 13:37:20 +0400

    refactor: clean up Vite configuration and improve main.tsx formatting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(),react()],
-  base: "/tonapi-dapp-example/"
+  plugins: [nodePolyfills(),react()]
 })
